d72f68c13134e42f53947480346759e27fbc12d1
Dakshh <dakshhsingh4902@gmail.com>
2023-05-31 23:13:34 +0530

    file modified

--- loops2.java
+++ loops2.java
@@ -2,5 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class loops2 {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            javaLoops(a, b, n);
+        }
+        in.close();
+    }
     
+    public static void javaLoops(int a, int b, int n){
+    int resultExpression = a + b;
+    String result = "" + resultExpression;
+    for(int i = 1; i < n; i++){
+        resultExpression += (Math.pow(2, i) * b);
+        result += " " + resultExpression;
+    }
+    System.out.println(result);
+    }
 }
