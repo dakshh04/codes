26cd8736d56dc2206ada27619377efb96ed44c21
Dakshh <dakshhsingh4902@gmail.com>
2023-06-01 23:19:19 +0530

    file modified

--- end_of_file.java
+++ end_of_file.java
@@ -2,5 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class end_of_file {
-    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
 }
