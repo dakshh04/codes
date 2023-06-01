4e01156284656b48c07ea46da74bef0e18453b54
Dakshh <dakshhsingh4902@gmail.com>
2023-06-01 23:25:54 +0530

    file modified

--- static_block.java
+++ static_block.java
@@ -6,5 +6,16 @@ public class static_block{
         Scanner input = new Scanner(System.in);
         int B = input.nextInt();
         int H = input.nextInt();
+        try{
+            if(B<=0 || H<=0){
+                throw new Exception();
+            }
+            else{
+                int area = B*H;
+                System.out.println(area);    
+            }
+        }catch(Exception e){
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
     }
 }
