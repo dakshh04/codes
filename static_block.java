8d836756d21a979f881a7dbb20c7598f2a6ebfa1
Dakshh <dakshhsingh4902@gmail.com>
2023-06-01 23:25:19 +0530

    file modified

--- static_block.java
+++ static_block.java
@@ -1,2 +1,10 @@
 import java.io.*;
 import java.util.*;
+
+public class static_block{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int B = input.nextInt();
+        int H = input.nextInt();
+    }
+}
