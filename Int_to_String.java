1aae56e5973da21b4d8f51850ee6d6dbcc54610b
Dakshh <dakshhsingh4902@gmail.com>
2023-06-03 00:36:13 +0530

    file added

---
+++ Int_to_String.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.security.*;
+public class Int_to_String {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
