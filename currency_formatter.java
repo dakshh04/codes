e2789f9d9b50b3fbc05657a7c02593a8c4a47cb0
Dakshh <dakshhsingh4902@gmail.com>
2023-06-04 14:00:42 +0530

    file modified

--- currency_formatter.java
+++ currency_formatter.java
@@ -8,3 +8,24 @@ public class currency_formatter {
     
 }
 
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        Locale i = new Locale("en", "IN");
+        
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india= NumberFormat.getCurrencyInstance(i).format(payment);
+        String china= NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france= NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
