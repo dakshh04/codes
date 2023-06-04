50918a24b2b8bd7a978144bb97c65093a5318d6a
Dakshh <dakshhsingh4902@gmail.com>
2023-06-04 13:53:51 +0530

    file modified

--- date_time.java
+++ date_time.java
@@ -4,6 +4,24 @@ import java.util.Date;
 import java.time.LocalDate; 
 import java.util.Scanner;
 
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        return LocalDate.of(year, month, day).getDayOfWeek().name();
+    }
+
+}
+
 public class date_time {
     
 }
