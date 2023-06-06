b258a1bd1184c30e630721f1e9fe6af8f3158b23
Dakshh <dakshhsingh4902@gmail.com>
2023-06-06 23:23:38 +0530

    file modified

--- substring.java
+++ substring.java
@@ -5,5 +5,11 @@ import java.math.*;
 import java.util.regex.*;
 
 public class substring {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start,end));
+    }   
 }
