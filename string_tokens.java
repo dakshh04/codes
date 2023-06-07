6989ef52ed93b43df5a748861bc365fce2b4c358
Dakshh <dakshhsingh4902@gmail.com>
2023-06-07 22:25:09 +0530

    file

--- string_tokens.java
+++ string_tokens.java
@@ -2,14 +2,5 @@ import java.io.*;
 import java.util.*;
 
 public class string_tokens {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        StringTokenizer st = new StringTokenizer(s," !,?._'@");
-        System.out.println(st.countTokens());
-        while(st.hasMoreTokens()){
-            System.out.println(st.nextToken());
-        }
-        scan.close();
-    }
+    
 }
