82c30b7d11b341f79f4790c61744d2b560a5d6c2
Dakshh <dakshhsingh4902@gmail.com>
2023-06-07 22:19:52 +0530

    file modified

--- string_reverse.java
+++ string_reverse.java
@@ -2,5 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class string_reverse{
-
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+    }
 }
