0d71bf451030d599460de3dae5dc7bf79db09b12
Dakshh <dakshhsingh4902@gmail.com>
2023-06-08 22:49:54 +0530

    file modified

--- syntax_checker.java
+++ syntax_checker.java
@@ -2,5 +2,12 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class syntax_checker {
-    
+    public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String pattern = in.nextLine();
+          	//Write your code
+		}
+	}
 }
