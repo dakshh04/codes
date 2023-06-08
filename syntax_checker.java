777132429928ca5c06764f4bdb3abbffcc58cf3d
Dakshh <dakshhsingh4902@gmail.com>
2023-06-08 22:52:39 +0530

    file modified

--- syntax_checker.java
+++ syntax_checker.java
@@ -4,12 +4,12 @@ import java.util.regex.*;
 
 // public class started
 public class syntax_checker {
+    // main function initiated
     public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		int testCases = Integer.parseInt(in.nextLine());
 		while(testCases>0){
 			String pattern = in.nextLine();
-          	//Write your code
 		}
 	}
 }
