0a9acf3a2d7b22db291f84e74ad9150407068adf
Dakshh <dakshhsingh4902@gmail.com>
2023-06-10 18:58:36 +0530

    file modified

--- regex.java
+++ regex.java
@@ -1,5 +1,6 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.*;
 
 class Solution{
 
@@ -14,5 +15,5 @@ class Solution{
 }
 
 public class regex {
-    
+    public String pattern = "(([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])";
 }
