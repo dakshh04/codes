4d482258a5e92c216c211e18ff921ee250d06ed1
Dakshh <dakshhsingh4902@gmail.com>
2023-06-10 18:57:07 +0530

    file modified

--- regex.java
+++ regex.java
@@ -1,6 +1,18 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
 public class regex {
     
 }
