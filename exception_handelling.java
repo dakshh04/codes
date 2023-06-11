2106945214eb2b771147dac14205d0c1dd04a2dd
Dakshh <dakshhsingh4902@gmail.com>
2023-06-11 22:33:18 +0530

    file modified

--- exception_handelling.java
+++ exception_handelling.java
@@ -1,9 +1,6 @@
 import java.util.*;
 
 class MyCalculator {
-    /*
-    * Create the method long power(int, int) here.
-    */
     public long power(int n, int p) throws Exception{
         if(n == 0 && p == 0) throw new Exception("n and p should not be zero.");
         if(n<0 || p<0) throw new Exception("n or p should not be negative.");
