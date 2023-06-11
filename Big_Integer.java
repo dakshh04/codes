93b71c29d19f1fab8494fba37d6b117dc250507b
Dakshh <dakshhsingh4902@gmail.com>
2023-06-11 22:24:53 +0530

    file modified

--- Big_Integer.java
+++ Big_Integer.java
@@ -4,5 +4,12 @@ import java.util.*;
 
 // public class start
 public class Big_Integer {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        
+        System.out.println(a + b);
+        System.out.println(a * b);
+    }
 }
