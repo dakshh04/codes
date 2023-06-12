8d2b98bf02d139d8a2876cc62b4474e2eb637823
Dakshh <dakshhsingh4902@gmail.com>
2023-06-12 23:52:03 +0530

    file added

--- linked_list.java
+++ linked_list.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 
-public class linked_list {
-    
+public class linked_list{
+    // create the node 
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
 }
+
