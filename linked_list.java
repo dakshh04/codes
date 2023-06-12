39813cc194b907724c6106c5f435ed12dfe2b799
Dakshh <dakshhsingh4902@gmail.com>
2023-06-12 23:53:01 +0530

    file added

--- linked_list.java
+++ linked_list.java
@@ -12,3 +12,20 @@ public class linked_list{
     }
 }
 
+class LinkedList{
+    Node head;
+
+    public void addNode( int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head =newNode;
+
+        }else{
+            Node current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+}
