7c5704694ab016ada5a6e2bcdc4db0b75fcce16b
Dakshh <dakshhsingh4902@gmail.com>
2023-06-14 23:20:06 +0530

    file modified

--- linear_search.java
+++ linear_search.java
@@ -8,3 +8,12 @@ int search(int arr[], int n, int x)
     }
     return -1;
 }
+
+int main(){
+ 	int arr[]={2,8,12,9};
+ 	int x=12;
+ 	int n=sizeof(arr)/sizeof(arr[0]);
+ 	printf("%d is present in %d index",x,search(arr,n,x));
+ 	getchar();
+ 	return 0;
+}
