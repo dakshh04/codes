d3a2e5b9c6f6a2d8c5672a762a3b9a49c9a3dd14
Dakshh <dakshhsingh4902@gmail.com>
2023-06-14 23:10:53 +0530

    file modified

--- two_sum.java
+++ two_sum.java
@@ -13,24 +13,25 @@ class Solution {
     }
 }
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numMap = new HashMap<>();
-        int n = nums.length;
+// // hash table
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         Map<Integer, Integer> numMap = new HashMap<>();
+//         int n = nums.length;
 
-        // Build the hash table
-        for (int i = 0; i < n; i++) {
-            numMap.put(nums[i], i);
-        }
+//         // Build the hash table
+//         for (int i = 0; i < n; i++) {
+//             numMap.put(nums[i], i);
+//         }
 
-        // Find the complement
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
-                return new int[]{i, numMap.get(complement)};
-            }
-        }
+//         // Find the complement
+//         for (int i = 0; i < n; i++) {
+//             int complement = target - nums[i];
+//             if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+//                 return new int[]{i, numMap.get(complement)};
+//             }
+//         }
 
-        return new int[]{}; // No solution found
-    }
-}
+//         return new int[]{}; // No solution found
+//     }
+// }
