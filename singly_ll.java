9030b3330179648c79fe08efb0aad1d9d9da2a44
Dakshh <dakshhsingh4902@gmail.com>
2023-06-15 23:36:21 +0530

    modified

--- singly_ll.java
+++ singly_ll.java
@@ -17,4 +17,17 @@ class LinkedList {
         this.head = null;
     }
 
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
     
