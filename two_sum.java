610d33d10fc009b725ad7d8d55f0c809ba7a36ac
Dakshh <dakshhsingh4902@gmail.com>
2023-06-16 23:31:09 +0530

    file modified

--- two_sum.java
+++ two_sum.java
@@ -14,6 +14,7 @@ class Solution {
 }
 
 //  hash table
+// implementing hash-maps
 // class Solution {
 //     public int[] twoSum(int[] nums, int target) {
 //         Map<Integer, Integer> numMap = new HashMap<>();
