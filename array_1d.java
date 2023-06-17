c9d6060cab039c5560b0aa304ac80b1097f8b83b
Dakshh <dakshhsingh4902@gmail.com>
2023-06-17 22:16:26 +0530

    modified

--- array_1d.java
+++ array_1d.java
@@ -1,5 +1,11 @@
 import java.util.*;
 
 public class array_1d {
-
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        
+        
+    }
 }
