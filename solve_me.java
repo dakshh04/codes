dd3a3da75ae5e168bad1aa51171486bb0b4d4401
Dakshh <dakshhsingh4902@gmail.com>
2023-06-21 22:50:02 +0530

    file modified

--- solve_me.java
+++ solve_me.java
@@ -11,5 +11,14 @@ public class Solution {
         sum = a + b;
     }
 
- 
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int a;
+            a = in.nextInt();
+            int b;
+            b = in.nextInt();
+            int sum;
+            sum = solveMeFirst(a, b);
+            System.out.println(sum);
+    }
 }
