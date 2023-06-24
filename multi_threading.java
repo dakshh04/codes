fce2fe0004ada7fa5fd51496944a2dcf2bc612af
Dakshh <dakshhsingh4902@gmail.com>
2023-06-24 22:07:25 +0530

    modified

--- multi_threading.java
+++ multi_threading.java
@@ -8,4 +8,14 @@ public class multi_threading {
             System.out.println("Exit from A");
         }
     }
+
+    static class B extends Thread{
+        public void run(){
+            for (int j = 1; j <= 5; j++) {
+                System.out.println("\t From Thread B : j = " + j);
+            }
+            System.out.println("Exit from B");
+        }
+    }
+
 }
