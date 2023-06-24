d443def09dc9c9b3de85779528b91c46ab22222f
Dakshh <dakshhsingh4902@gmail.com>
2023-06-24 22:17:38 +0530

    file modified

--- array_traverse.java
+++ array_traverse.java
@@ -1,3 +1,12 @@
 public class array_traverse {
-    
+    public static void main(String[] args) {
+        int arr[] = {12, 23, 34, 32, 45, 54, 67, 16, 26, 27, 21, 51};
+        int n = arr.length;
+        int x = 67;
+        for(int i = 0; i < n; i++){
+            if(arr[i] == x){
+                System.out.println(i);
+            }
+        }
+    }
 }
