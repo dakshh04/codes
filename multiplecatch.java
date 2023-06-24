10f454a830a0031d0f1b3765b19e5882de230fc0
Dakshh <dakshhsingh4902@gmail.com>
2023-06-24 22:15:14 +0530

    modified

--- multiplecatch.java
+++ multiplecatch.java
@@ -1,5 +1,18 @@
 import java.util.*;
 
 public class multiplecatch {
-    
+    Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Enter a number: ");
+            int num = scanner.nextInt();
+            int result = 100 / num;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            scanner.close();
+            System.out.println("Scanner closed.");
+        }
 }
