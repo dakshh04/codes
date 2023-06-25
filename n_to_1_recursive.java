480075eee22b319e6ca094102944dbb05ea81a0c
Dakshh <dakshhsingh4902@gmail.com>
2023-06-25 17:53:36 +0530

    file added

---
+++ n_to_1_recursive.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+// Normal Method
+// public class n_to_1_recursive {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i = n; i>0 ; i--){
+//             System.out.println(i);
+//         }
+
+//     }
+// }
+
+// Recursion Method
+class n_to_1_recursive{
+
+    static void printNto1(int n){
+        if (n ==0 ){
+            System.out.println("0 value");
+            return;
+        }
+        else{
+            System.out.println(n);
+            printNto1(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 0;
+        printNto1(n);
+    }
+}
