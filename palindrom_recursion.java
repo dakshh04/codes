d126019f0c38916fcc479c26a48f7a19a2e4a020
Dakshh <dakshhsingh4902@gmail.com>
2023-06-27 16:41:12 +0530

    file modified

--- palindrom_recursion.java
+++ palindrom_recursion.java
@@ -9,6 +9,7 @@ public class palindrom_recursion {
     // public static void main(String[] args) {
     //     isPalindrome("GFG", 1, 3);
     // }
+    
     static boolean palRec(String str, int s, int e){
         if(s==e){
             return true;
@@ -20,5 +21,24 @@ public class palindrom_recursion {
             return palRec(str, s + 1, e -1 );
         }
         return true;
+    }
+
+    static boolean isPalindrome(String str){
+        int n = str.length();
+
+        if(n == 0) return true;
+
+        return palRec(str, 0, n-1);
+    }
+
+    public static void main(String[] args) {
+        String str = "gefgeg";
+
+        if(isPalindrome(str)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
     }
 }
