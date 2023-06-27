ebed22616ffdafd87a77b6424a48f76047986819
Dakshh <dakshhsingh4902@gmail.com>
2023-06-27 16:37:55 +0530

    modified

--- palindrom_recursion.java
+++ palindrom_recursion.java
@@ -1,12 +1,24 @@
 public class palindrom_recursion {
-    public static boolean isPalindrome(String str, int start, int end){
-        if(start >= end){
+    // public static boolean isPalindrome(String str, int start, int end){
+    //     if(start >= end){
+    //         return true;
+    //     }
+    //     return (str.charAt(start) == str.charAt(end)) && isPalindrome(str, start + 1, end + 1);
+    // }
+
+    // public static void main(String[] args) {
+    //     isPalindrome("GFG", 1, 3);
+    // }
+    static boolean palRec(String str, int s, int e){
+        if(s==e){
             return true;
         }
-        return (str.charAt(start) == str.charAt(end)) && isPalindrome(str, start + 1, end + 1);
-    }
-
-    public static void main(String[] args) {
-        isPalindrome("GFG", 1, 3);
+        if(str.charAt(s) != str.charAt(e)){
+            return false;
+        }
+        if(s < e+1){
+            return palRec(str, s + 1, e -1 );
+        }
+        return true;
     }
 }
