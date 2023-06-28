bcfd3038a44ad8c7f45a38e3be1744a9e709372f
Dakshh <dakshhsingh4902@gmail.com>
2023-06-28 10:17:33 +0530

    file modified

--- sum_of_digits.java
+++ sum_of_digits.java
@@ -1,12 +1,22 @@
 class sum_of_digits{
-    static int getSum (int n){
-        if(n == 0){
-            return 0;
+    // static int getSum (int n){
+    //     if(n == 0){
+    //         return 0;
+    //     }
+    //     else{
+    //         return getSum(n/10) + n%10;
+    //     }
+    // }
+
+    static int getSum(int n){
+        int res = 0;
+        while(res >= 0){
+            res = res + n%10;
+            n = n/10;
         }
-        else{
-            return getSum(n/10) + n%10;
-        }
+        return res;
     }
+
     public static void main(String[] args) {
         int n = 2535;
         System.out.println(getSum(n));
