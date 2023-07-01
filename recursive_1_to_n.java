b0143f2ae44bf0497994b5bfd5d9951f34472353
Dakshh <dakshhsingh4902@gmail.com>
2023-07-01 11:41:27 +0530

    file modified

--- recursive_1_to_n.java
+++ recursive_1_to_n.java
@@ -1,4 +1,5 @@
 public class recursive_1_to_n {
+    // One Way
     static void print1toN(int n){
         if(n == 0){
             return;
@@ -7,6 +8,15 @@ public class recursive_1_to_n {
             print1toN(n-1);
             System.out.println(n);
         }
+    }
+
+    // Second Way
+    static void print_1toN(int n1, int n2){
+        if(n1 <= n2){
+            System.out.println(n1 + " ");
+            print_1toN( n1 + 1, n2);
+        }
+
     }
     public static void main(String[] args) {
         int n = 5;
