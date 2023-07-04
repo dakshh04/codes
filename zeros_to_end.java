66361096b2d767ef66f98f03e4c994a71862d42c
Dakshh <dakshhsingh4902@gmail.com>
2023-07-04 14:27:49 +0530

    file added

---
+++ zeros_to_end.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class zeros_to_end {
+    static void moveZeroes(int arr []){
+        int n = arr.length;
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] != 0){
+                arr[i] = arr[count];
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr [] = {12, 13, 14, 0, 0, 15, 0, 16, 0, 0, 17};
+        moveZeroes(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
