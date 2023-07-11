e2fcd78281e3fec7aa9b974cb0288d644a34c6a6
Dakshh <dakshhsingh4902@gmail.com>
2023-07-11 14:24:55 +0530

    file modified

--- binary_search_rec.java
+++ binary_search_rec.java
@@ -1,6 +1,6 @@
 public class binary_search_rec {
     static int binarySearch(int arr[], int x, int low, int high){
-        if(high > low){
+        if(high >= low){
             int mid = (low+high)/2;
             if(arr[mid] == x){
                 return mid;
