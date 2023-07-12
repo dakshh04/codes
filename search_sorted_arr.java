0939db9dab9d8d0461c65ddb4b982264cb1d0312
Dakshh <dakshhsingh4902@gmail.com>
2023-07-12 12:21:16 +0530

    file modified

--- search_sorted_arr.java
+++ search_sorted_arr.java
@@ -2,20 +2,33 @@ public class search_sorted_arr {
     static int search(int arr[], int x){
         int n = arr.length;
         int low = 0, high = n-1;
-        int mid = (low + high)/2;
-        if(arr[low] < arr[mid]){
-            if(x < arr[mid] && x > arr[low]){
-                high = mid - 1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] == x){
+                return mid;
+            }
+            if(arr[low] <= arr[mid]){
+                if(x < arr[mid] && x >= arr[low]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
             }
             else{
-                low = mid + 1;
+                if(x > arr[mid] && x <= arr[high]){
+                    low = mid +1;
+                }
+                else{
+                    high = mid - 1;
+                }
             }
         }
-        else{
-            if(x > arr[mid] && x < arr[high]){
-                low = mid +1;
-            }
-        }
-        return -1;
+        return -1; 
+    }
+
+    public static void main(String[] args) {
+        int arr [] = {10, 20, 40, 60, 5, 8};
+        System.out.println(search(arr, 5));
     }
 }
