ef5241ac6549b37a19ebd0537b59fcd9fa2e1fbb
Dakshh <dakshhsingh4902@gmail.com>
2023-07-12 19:18:24 +0530

    file added

--- two_pointer_arr.java
+++ two_pointer_arr.java
@@ -2,10 +2,10 @@ public class two_pointer_arr {
     static boolean twoPointer(int arr [], int x){
         int n = arr.length;
         int i = 0, j = n-1;
-        if(arr[i] + arr[j] == x){
-            return true;
-        }
         while(i < j){
+            if(arr[i] + arr[j] == x){
+                return true;
+            }
             if(arr[i] + arr[j] > x){
                 j -= 1;
             }
@@ -17,7 +17,7 @@ public class two_pointer_arr {
     }
 
     public static void main(String[] args) {
-        int arr [] = {2, 4, 6, 8, 10, 12, 13, 15, 16, 19};
+        int arr [] = {2, 4, 6, 8, 11, 12, 13, 15, 17, 19};
         System.out.println(twoPointer(arr, 22));
     }
 }
