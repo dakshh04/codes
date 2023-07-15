24d5efbfafd14b32389d6ee907efb010551ecf48
Dakshh <dakshhsingh4902@gmail.com>
2023-07-15 21:46:19 +0530

    file added

---
+++ insertion_sort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class insertion_sort{
+    static void insertionSort(int arr []){
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr [] = {50, 30, 20, 10, 39, 25, 27, 167, 67, 16};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
