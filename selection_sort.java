5b1dadfa22d31650481b1d82b734fd287c0e5aa8
Dakshh <dakshhsingh4902@gmail.com>
2023-07-15 12:00:56 +0530

    file added

---
+++ selection_sort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class selection_sort {
+    static void selectSort(int arr []){
+        int n = arr.length;
+        for(int i = 0; i < n-1; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < n; j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {64,25,12,22,11};
+        selectSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
