eabee2649932e946a2c87bc211dc4f4ef59bbfda
Dakshh <dakshhsingh4902@gmail.com>
2023-07-17 10:38:37 +0530

    file added

---
+++ intersection_arrays.java
@@ -0,0 +1,30 @@
+public class intersection_arrays {
+    static void intersection(int a[], int b[]){
+        int n = a.length;
+        int m = b.length;
+        int i = 0, j = 0;
+        while(i < n && j < m){
+            if(i > 0 && a[i] == a[i-1]){
+                i++;
+                continue;
+            }
+            if(a[i] > b[j]){
+                j++;
+            }
+            else if(a[i] < b[j]){
+                i++;
+            }
+            else{
+                System.out.println(a[i]);
+                i++;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[] = {10, 20, 20, 40, 60};
+        int b[] = {22, 20, 20, 20};
+        intersection(a, b);
+    }
+}
