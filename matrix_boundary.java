dd7c19393f91736767a04b33d19c3f4a16565581
Dakshh <dakshhsingh4902@gmail.com>
2023-07-18 10:10:48 +0530

    file added

--- matrix_boundary.java
+++ matrix_boundary.java
@@ -1,37 +1,35 @@
-public class matrix_boundary {
-    static void matrix_boundary(int arr[][], int m, int n){
-        if(m == 1){
-            for(int i = 0; i < n; i++){
-                System.out.print(arr[0][i] + " ");
+import java.io.*;
+public class GFG {
+ 
+    public static void printBoundary(int a[][], int m,
+                                     int n)
+    {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0)
+                    System.out.print(a[i][j] + " ");
+                else if (i == m - 1)
+                    System.out.print(a[i][j] + " ");
+                else if (j == 0)
+                    System.out.print(a[i][j] + " ");
+                else if (j == n - 1)
+                    System.out.print(a[i][j] + " ");
+                else
+                    System.out.print("  ");
             }
-        }
-        if(n == 1){
-            for(int i = 0; i < m; i++){
-                System.out.print(arr[i][0] + " ");
-            }
-        }
-        else{
-            for(int i = 0; i < n; i++){
-                System.out.print(arr[0][i] + " ");
-            }
-            for(int i = 1; i < m; i++){
-                System.out.print(arr[i][n-1] + " ");
-            }
-            for(int i = n-2; i >= 0; i--){
-                System.out.print(arr[m-1][i] + " ");
-            }
-            for(int i = m; i >= 1; i--){
-                System.out.print(arr[i][0] + " ");
-            }
+            System.out.println("");
         }
     }
-
-    public static void main(String[] args) {
+ 
+    /* Driver code */
+    public static void main(String[] args)
+    {
         int a[][] = { { 1, 2, 3, 4 },
                       { 5, 6, 7, 8 },
                       { 1, 2, 3, 4 },
                       { 5, 6, 7, 8 } };
-        
-        matrix_boundary(a, 4, 4);
+ 
+        // Function call
+        printBoundary(a, 4, 4);
     }
 }
