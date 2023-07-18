8b023332629f699469c3ee9b94704bcb2bac1b23
Dakshh <dakshhsingh4902@gmail.com>
2023-07-18 15:48:37 +0530

    file added

---
+++ transpose_matrix.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class transpose_matrix {
+    static void transpose(int arr[][], int n, int m){
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < m; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = { { 1, 1, 1, 1 },
+                      { 2, 2, 2, 2 },
+                      { 3, 3, 3, 3 },
+                      { 4, 4, 4, 4 } }; 
+
+        transpose(arr, 4, 4);
+        
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
