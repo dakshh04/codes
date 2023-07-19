f461bf60ee7e4147e0bc39777efed9a936cd52d6
Dakshh <dakshhsingh4902@gmail.com>
2023-07-19 15:30:25 +0530

    file modified

--- frequency_elements.java
+++ frequency_elements.java
@@ -4,7 +4,7 @@ public class frequency_elements {
     static void countFreq(int arr []){
         HashMap<Integer, Integer> h = new HashMap<>();
         for(int x : arr){
-            h.put(x, h.getOrDefault(h, 0) + 1);
+            h.put(x, h.getOrDefault(x, 0) + 1);
         }
         for(Map.Entry<Integer, Integer> e : h.entrySet()){
             System.out.println(e.getKey() + " " + e.getValue());
