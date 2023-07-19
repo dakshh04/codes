3571f80658eeea121862e4267bf5bcd343d27b9c
Dakshh <dakshhsingh4902@gmail.com>
2023-07-19 15:29:14 +0530

    file added

---
+++ frequency_elements.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class frequency_elements {
+    static void countFreq(int arr []){
+        HashMap<Integer, Integer> h = new HashMap<>();
+        for(int x : arr){
+            h.put(x, h.getOrDefault(h, 0) + 1);
+        }
+        for(Map.Entry<Integer, Integer> e : h.entrySet()){
+            System.out.println(e.getKey() + " " + e.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr [] = {50, 50, 10, 40, 10};
+        countFreq(arr);
+    }
+}
