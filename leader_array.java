c3b2188ebde37d4288e9f3c7e276c382cb3f53f2
Dakshh <dakshhsingh4902@gmail.com>
2023-07-25 12:39:28 +0530

    file added

---
+++ leader_array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class leader_array {
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int leader = arr[n - 1];
+        list.add(leader);
+        
+        for(int i = n - 2; i >= 0; i--){
+            
+        }
+    }
+}
