3cf4b109e0238b4324583fac15808e75cdc78b34
Dakshh <dakshhsingh4902@gmail.com>
2023-07-26 11:30:50 +0530

    modified

--- ll_recursive_traverse.java
+++ ll_recursive_traverse.java
@@ -1,24 +1,33 @@
-class Node{
+class Node {
     int data;
     Node next;
 
-    public Node(int data, Node next){
+    public Node(int data) {
         this.data = data;
-        this.next = next;
+        this.next = null;
     }
 }
 
 public class ll_recursive_traverse{
-    public static void printList(Node head){
-        if(head == null){
+    public static void printList(Node head) {
+        if (head == null) {
             return;
         }
-        System.out.println(head.next);
+        System.out.println(head.data);
         printList(head.next);
     }
-
+    // public Node addNode(Node head){
+    //     if(head == null)
+    //      = new Node(2,head);
+    //     return head;
+    // }
     public static void main(String[] args) {
-        Node head = new Node(1, new Node(2, new Node(3, null)));
+        // Node head = new Node(1, new Node(2, new Node(3)));
+        // Node head = new  Node(1,null);
+        // head = addNode(head);
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
         printList(head);
     }
 }
