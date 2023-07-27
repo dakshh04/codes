4a5076994ab3c666fbcffa303d7b698d4ac097b5
Dakshh <dakshhsingh4902@gmail.com>
2023-07-27 10:32:56 +0530

    file added

---
+++ insert_at_pos.java
@@ -0,0 +1,58 @@
+class Node{
+    int data;
+    Node next;
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+
+class LinkedList{
+    Node insertAtPos(Node head, int x, int pos){
+        Node temp = new Node(x);
+        if(head == null){
+            if(pos == 1){
+                return temp;
+            }
+            else{
+                return head;
+            }
+        }
+        
+        if(pos == 1){
+            temp.next = head;
+            return temp;
+        }
+        
+        Node curr = head;
+        for(int i = 1; i < pos - 1; i++){
+            curr = curr.next;
+            if(curr == null){
+                System.out.println("Out of range.");
+                return head;
+            }
+        }
+        temp.next = curr.next;
+        curr.next = temp;
+        return head;
+    }
+    
+    void printList(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String [] args){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        LinkedList ll = new LinkedList();
+        ll.printList(head);
+        head = ll.insertAtPos(head, 25, 2);
+        ll.printList(head);
+    }
+}
