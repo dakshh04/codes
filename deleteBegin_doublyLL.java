dfe55ef7a1e9a86d6e9c4dd3e00652bb84346aba
Dakshh <dakshhsingh4902@gmail.com>
2023-07-28 17:55:53 +0530

    file added

---
+++ deleteBegin_doublyLL.java
@@ -0,0 +1,43 @@
+class Node{
+    int data;
+    Node prev;
+    Node next;
+    Node(int x){
+        data = x;
+        prev = null;
+        next = null;
+    }
+}
+
+class Main{
+    static Node deleteBegin(Node head){
+        if(head == null || head.next == null) return null;
+        else{
+            head = head.next;
+            head.prev = null;
+            return head;
+        }
+    }
+    
+    static void printList(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String [] args){
+        Node head = new Node(10);
+        Node temp1 = new Node(20);
+        Node temp2 = new Node(30);
+        head.next = temp1;
+        temp1.prev = head;
+        temp1.next = temp2;
+        temp2.prev = temp1;
+        printList(head);
+        head = deleteBegin(head);
+        printList(head);
+    }
+}
