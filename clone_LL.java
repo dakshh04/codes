92273c57423c3a365b87725d7e0b16c0511b99f8
Dakshh <dakshhsingh4902@gmail.com>
2023-08-02 12:19:23 +0530

    file added

---
+++ clone_LL.java
@@ -0,0 +1,16 @@
+class Node{
+    int data;
+    Node next, random;
+    Node(int x){
+        data = x;
+        next = random = null;
+    }
+}
+
+public class clone_LL {
+    static Node clone(Node head){
+        Node next, temp;
+
+        for(Node curr = head; curr!=null; curr)
+    }
+}
