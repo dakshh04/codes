8e9e095670b5e9277fa6e7879f20341187c8f776
Dakshh <dakshhsingh4902@gmail.com>
2023-08-06 22:31:58 +0530

    file added

---
+++ stringPermutations.java
@@ -0,0 +1,10 @@
+public class stringPermutations {
+    static void permute(String str, int l, int r){
+        if(l==r){
+            System.out.println(str);
+            return;
+        }
+
+        
+    }
+}
