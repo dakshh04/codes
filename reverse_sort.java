ef02ba05dcbec697e68b7e4343b191226389ee2b
Dakshh <dakshhsingh4902@gmail.com>
2023-08-15 23:08:56 +0530

    file added

--- reverse_sort.java
+++ reverse_sort.java
@@ -2,8 +2,9 @@ import java.util.*;
 
 class reverse_sort{
     public static void main(String[] args) {
-        Integer arr[] = {2, 6, 12, 4, 3, 7, 17, 9};
+        Integer arr[] = {2, 6, 12, 4, 3, 1, 7, 17};
         Arrays.sort(arr, Collections.reverseOrder());
         System.out.println(Arrays.toString(arr));
     }
+
 }
