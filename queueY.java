9e364ed7f0d1966d3737671e5f85a17c0e17b878
Dakshh <dakshhsingh4902@gmail.com>
2023-09-06 17:53:00 +0530

    file added

---
+++ queueY.java
@@ -0,0 +1,11 @@
+public class queueY{
+    static class Queue{
+        static int arr[];
+        static int size;
+
+        Queue(int n){
+            arr = new int [n];
+            this.size = n;
+        }
+    }
+}
