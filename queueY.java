f017d82ddafb818c6b7ece0a777573c4cfcdc0e8
Dakshh <dakshhsingh4902@gmail.com>
2023-09-06 17:59:24 +0530

    modified

--- queueY.java
+++ queueY.java
@@ -2,10 +2,31 @@ public class queueY{
     static class Queue{
         static int arr[];
         static int size;
+        static int rear = -1;
 
         Queue(int n){
             arr = new int [n];
             this.size = n;
         }
+
+        public static boolean isEmpty(){
+            return rear == -1;
+        }
+
+        public static void add(int data){
+            if(rear==size - 1){
+                System.out.println("Full");
+                return;
+            }
+
+            rear++;
+            arr[rear] = data;
+        }
+
+
+    }
+
+    public static void main(String [] args){
+
     }
 }
