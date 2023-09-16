a00eabd8018c060770060571daa616cbd0fb08e0
Dakshh <dakshhsingh4902@gmail.com>
2023-09-16 19:40:29 +0530

    file added

--- levelOrder.java
+++ levelOrder.java
@@ -1,3 +1,27 @@
+import java.util.Queue;
+
+// level order traversal of a binary tree
 public class levelOrder {
-    
+    public static void levelOrdertraversal(Node root){
+        if(root == null){
+            return;
+        }
+        Queue<Node> q = new java.util.LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(q.size()>1){
+            Node curr = q.poll();
+            if(curr==null){
+                System.out.println();
+                q.add(null);
+                continue;
+            }
+            System.out.print(curr.data + " ");
+            if(curr.left!=null){
+                q.add(curr.left);
+            }
+            if(curr.right!=null){
+                q.add(curr.right);
+            }
+        }
 }
