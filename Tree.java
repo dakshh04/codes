3d138c2b37f9689571d7bfbaccf2f3de0ab24b33
Dakshh <dakshhsingh4902@gmail.com>
2023-09-19 19:03:04 +0530

    modified

--- Tree.java
+++ Tree.java
@@ -87,10 +87,6 @@ public class Tree {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        // System.out.println(root.data);
-        // preOrder(root);
-        // inOrder(root);
-        // postOrder(root);
         levelOrder(root);
     }
 }
