6930d3f2a91e28892552508457048159b2ea4a1d
Dakshh <dakshhsingh4902@gmail.com>
2023-12-13 10:23:55 +0530

    file modified

--- consecutive1's.java
+++ consecutive1's.java
@@ -1,1 +1,18 @@
 import java.util.*;
+
+// User function Template for Java
+
+class Solution {
+    long countStrings(int n) {
+        long mod = (long) 1e9 + 7;
+        long a[] = new long[n], b[] = new long[n];
+        a[0] = b[0] = 1;
+        
+        for(int i = 1; i < n; i++) {
+            a[i] = (a[i - 1] + b[i - 1]) % mod;
+            b[i] = a[i - 1] % mod;
+        }
+        
+        return (a[n - 1] + b[n - 1]) % mod;
+    }
+}
