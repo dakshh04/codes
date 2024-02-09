24c84c4168095adc24cef1c1c187f5ed29965419
Dakshh <dakshhsingh4902@gmail.com>
2024-02-09 23:08:25 +0530

    file added

--- ser_deSer_BT.java
+++ ser_deSer_BT.java
@@ -1,4 +1,38 @@
 class Tree 
 {
-    
+    //Function to serialize a tree and return a list containing nodes of tree.
+    static final int EMPTY = -1;
+	public void serialize(Node root, ArrayList<Integer> arr) 
+	{
+	    //code here
+	    if(root == null){
+	        arr.add(EMPTY);
+	        return;
+	    }
+	    
+	    arr.add(root.data);
+	    serialize(root.left, arr);
+	    serialize(root.right, arr);
+	}
+	
+	//Function to deserialize a list and construct the tree.
+	int index = 0;
+    public Node deSerialize(ArrayList<Integer> arr)
+    {
+        //code here
+        if(index == arr.size()){
+            return null;
+        }
+        
+        int val = arr.get(index);
+        index++;
+        
+        if(val == EMPTY) return null;
+        
+        Node root = new Node(val);
+        root.left = deSerialize(arr);
+        root.right = deSerialize(arr);
+        return root;
+        
+    }
 };
